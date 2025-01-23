cdce410ed1c4506beeaa57bf70acb6c427788c6f
AntiDivis <134837223+AntiDivis@users.noreply.github.com>
2025-01-23 14:01:34 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Baul_MyFirstApp {
+        public static void main(String[] args) {
+            LocalDate X = LocalDate.now();
+            LocalTime Y = LocalTime.now();
+            String S = "!!!";
+
+
+            System.out.println("My name is Jeric Owen S. Baul " + S); // Hello
+            System.out.println("The current date is: " + X.getDayOfYear() +" "+ X.getMonth() +" "+ X.getYear());
+            System.out.println("The current time is: " + Y.getHour() +":"+ Y.getMinute() +":"+ Y.getSecond());
+        }
+
     }
-}
+
+
